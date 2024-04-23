de3be4dfcc615199a4cf48a864124a737f44c17d
Emmanuel Hudson <adjetey545@gmail.com>
2024-04-23 09:09:28 -0700

    linear search intro

--- TreeHouse/linear_search.py
+++ TreeHouse/linear_search.py
@@ -1,2 +1,13 @@
 def linear_search(list, target):
-    None
+    """Returns The Index position of the target if found else returns None
+
+    Args:
+        list (array): 
+        A one-dimensional array or list.
+        target (Number or String): 
+        The element to be searched in the list.
+    """
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
